a610177dfdefd81dd3239e30d607ee9b0e08372b
Petro Korienev <soxjke@gmail.com>
2014-11-02 01:10:19 +0200

    Initial Commit

---
+++ TransparentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransparentControl
+//
+//  Created by Petro Korienev on 11/2/14.
+//  Copyright (c) 2014 Petro Korienev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransparentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransparentControl
+//
+//  Created by Petro Korienev on 11/2/14.
+//  Copyright (c) 2014 Petro Korienev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
